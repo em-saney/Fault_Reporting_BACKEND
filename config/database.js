3ea7bbed7c02170d5b2fedbb57c22dfce3abea2f
em-saney <ismailsanigambo@gmail.com>
2024-10-02 11:52:32 +0100

    changes on config file and added .env file

--- config/database.js
+++ config/database.js
@@ -1,21 +1,34 @@
 const { Sequelize } = require('sequelize');
 
-// Set up PostgreSQL connection using Sequelize
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'faultReportingSystem',  // Use DATABASE_URL in production, fallback to local DB
-  process.env.DATABASE_USER || 'admin',             // Use DATABASE_USER in local setup
-  process.env.DATABASE_PASSWORD || 'admin123',      // Use DATABASE_PASSWORD in local setup
-  {
-    host: process.env.DATABASE_HOST || 'localhost',    // Use DATABASE_HOST in local setup
+// Determine if running in production or development
+const isProduction = process.env.NODE_ENV === 'production';
+
+let sequelize;
+
+if (isProduction) {
+  const connectionString = 'postgres://admin:password@dpg-crui0rtumphs73enpdfg-a:5432/fault_reporting_system';
+
+  // Ensure the connection string is not null or undefined
+  if (!connectionString) {
+    throw new Error('Connection string is not defined.');
+  }
+
+  sequelize = new Sequelize(connectionString, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  // Local database connection
+  sequelize = new Sequelize('faultReportingSystem', 'admin', 'admin123', {
+    host: 'localhost',
     dialect: 'postgres',
     logging: false,
-    dialectOptions: {
-      ssl: process.env.DATABASE_URL ? {                // Enable SSL if using DATABASE_URL
-        require: true,
-        rejectUnauthorized: false,  // Adjust based on your SSL requirements
-      } : false,
-    },
-  }
-);
+  });
+}
 
 module.exports = sequelize;
